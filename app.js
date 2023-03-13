17d1d87816ce0308b286dffb24db07f6921f7368
Teletrup <saletrapotasowatosyf@gmail.com>
2023-03-13 22:06:54 +0100

    wave shape buttons (almost)

--- app.js
+++ app.js
@@ -36,23 +36,54 @@ function viewKnob(draw, knob) {
   );
 }
 
+//abstracting
+//wave name
+//drawing expression
+function waveButtonView(draw, wf, picture) {
+  return ['svg', {style: 'width: 30px; height: 20px', onclick: () => {waveform = wf; draw()}},
+    ['rect', {width: '100%', height: '100%', fill: ((waveform === wf) ? 'black' : '#eee')}],
+    ['svg', {x: 0.15*30, y: 0.15*20, width: 0.7*30, height: 0.7*20, stroke: (waveform === wf) ? 'white' : 'black'},
+      picture, //TODO use a group with scaling? //what's a viewbox?
+    ],
+  ];
+}
+
 const a = 0.2;
 const d = 0.2;
 const s = 0.6;
 const r = 0.3;
 
+let waveform = 'sawtooth';
+
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
 // create Oscillator node
-const master = audioCtx.createGain();
-master.gain.setValueAtTime(0.5, audioCtx.currentTime);
+const input = audioCtx.createGain();
+input.gain.value = 50;
+//^^
+
+const master = audioCtx.createDynamicsCompressor();
+master.threshold.value = -50;
+
+//master.gain.setValueAtTime(1, audioCtx.currentTime);
+
+
+const lop = audioCtx.createBiquadFilter();
+lop.type = 'lowpass';
+lop.frequency.value = 1000;
+//lop.Q.value = 50;
+
+
+input.connect(lop);
+lop.connect(master);
+master.connect(audioCtx.destination);
 
 
 
 let octave = 3;
 
 const noteNames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
-const noteKeys = 'zsxdcvgbhnjm,l.;/q2w3e4rt6y7ui9o0p-'
+const noteKeys = 'zsxdcvgbhnjm,l.;/q2w3e4rt6y7ui9o0p-[]'
 const keyFlags = Object.fromEntries([...noteKeys].map(x => [x, false]));
 
 const notes = Array.from(Array(128), (x, i) => ({
@@ -67,10 +98,10 @@ function getNoteName(n) {
 
 function view(draw) {
   document.body.onkeypress = e => {
-    if (e.key == '[') {
+    if (e.key == '<') {
       octave--;
       draw();
-    } else if (e.key == ']') {
+    } else if (e.key == '>') {
       octave++;
       draw();
     }
@@ -81,10 +112,9 @@ function view(draw) {
     keyFlags[relIdx] = true;
     const noteIdx = relIdx + (octave + 2) * 12;
     const osc = audioCtx.createOscillator();
-    osc.type = "sawtooth";
-    master.connect(audioCtx.destination);
+    osc.type = waveform;
     osc.frequency.setValueAtTime(440 * 2**((noteIdx - 69)/12), audioCtx.currentTime); // value in hertz
-    osc.connect(master);
+    osc.connect(input);
     osc.start();
     notes[noteIdx].osc = osc;
     draw();
@@ -95,7 +125,7 @@ function view(draw) {
     keyFlags[relIdx] = false;
     const noteIdx = relIdx + octave * 12 + 24;
     notes[noteIdx].osc.stop();
-    notes[noteIdx].osc.disconnect(master);
+    notes[noteIdx].osc.disconnect(input);
     notes[noteIdx].osc = undefined;
     draw();
   }
@@ -105,6 +135,38 @@ function view(draw) {
     `notes: ${notes.filter(x => x.osc !== undefined).map(x => x.name).join(', ')}`,
     //store names in array?
     //'a\na\n', //no nl-s in text nodes?
+    ['br'],
+    waveButtonView(draw, 'sine',
+      ['g',
+        /*TODO*/
+        ['line', {x1: '0%', y1: '50%', x2: '25%', y2: '0%'}],
+        ['line', {x1: '25%', y1: '0%', x2: '75%', y2: '100%'}],
+        ['line', {x1: '75%', y1: '100%', x2: '100%', y2: '50%'}],
+      ]
+    ),
+    waveButtonView(draw, 'triangle',
+      ['g',
+        ['line', {x1: '0%', y1: '50%', x2: '25%', y2: '0%'}],
+        ['line', {x1: '25%', y1: '0%', x2: '75%', y2: '100%'}],
+        ['line', {x1: '75%', y1: '100%', x2: '100%', y2: '50%'}],
+      ]
+    ),
+    waveButtonView(draw, 'square',
+      ['g',
+        ['line', {x1: '0%', y1: '50%', x2: '0%', y2: '100%'}],
+        ['line', {x1: '0%', y1: '100%', x2: '50%', y2: '100%'}],
+        ['line', {x1: '50%', y1: '100%', x2: '50%', y2: '0%'}],
+        ['line', {x1: '50%', y1: '0%', x2: '100%', y2: '0%'}],
+        ['line', {x1: '100%', y1: '0%', x2: '100%', y2: '50%'}],
+      ]
+    ),
+    waveButtonView(draw, 'sawtooth',
+      ['g',
+        ['line', {x1: '0%', y1: '50%', x2: '50%', y2: '0%'}],
+        ['line', {x1: '50%', y1: '0%', x2: '50%', y2: '100%'}],
+        ['line', {x1: '50%', y1: '100%', x2: '100%', y2: '50%'}],
+      ]
+    ),
   ];
 }
 
